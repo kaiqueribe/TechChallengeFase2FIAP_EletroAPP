68b4a8179d2b8117996cc6a7ba6a81f336956d7d
rodrigo.l-macedo <rodrigo.l-macedo@sefaz.pe.gov.br>
2023-07-05 21:34:22 -0300

    add uuid

--- src/main/java/com/fiap/grupo9/AppEletroControl/dominio/endereco/dto/EnderecoDTO.java
+++ src/main/java/com/fiap/grupo9/AppEletroControl/dominio/endereco/dto/EnderecoDTO.java
@@ -1,20 +1,36 @@
 package com.fiap.grupo9.AppEletroControl.dominio.endereco.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fiap.grupo9.AppEletroControl.dominio.endereco.entitie.Endereco;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.Getter;
+import lombok.Setter;
 
 
 import java.util.UUID;
-
+@Getter
+@Setter
 public class EnderecoDTO{
 
 
     private UUID id;
+    @JsonProperty
+    @NotBlank
+    @Pattern(regexp = "\\d{8}", message = "CEP inválido")
     private String cep;
+    @JsonProperty
+    @NotBlank
     private String end;
+    @JsonProperty
     private String numero;
-
+    @JsonProperty
+    @NotBlank
     private String bairro;
+    @JsonProperty
+    @NotBlank
     private String uf;
+    @JsonProperty
     private String complemento;
 
     public EnderecoDTO() {
@@ -38,68 +54,5 @@ public class EnderecoDTO{
         this.bairro=entidade.getBairro();
         this.uf=entidade.getUf();
         this.complemento=entidade.getComplemento();
-    }
-
-    public UUID getId() {
-        return id;
-    }
-
-    public EnderecoDTO setId(UUID id) {
-        this.id = id;
-        return this;
-    }
-
-    public String getCep() {
-        return cep;
-    }
-
-    public EnderecoDTO setCep(String cep) {
-        this.cep = cep;
-        return this;
-    }
-
-    public String getEnd() {
-        return end;
-    }
-
-    public EnderecoDTO setEnd(String end) {
-        this.end = end;
-        return this;
-    }
-
-    public String getNumero() {
-        return numero;
-    }
-
-    public EnderecoDTO setNumero(String numero) {
-        this.numero = numero;
-        return this;
-    }
-
-    public String getBairro() {
-        return bairro;
-    }
-
-    public EnderecoDTO setBairro(String bairro) {
-        this.bairro = bairro;
-        return this;
-    }
-
-    public String getUf() {
-        return uf;
-    }
-
-    public EnderecoDTO setUf(String uf) {
-        this.uf = uf;
-        return this;
-    }
-
-    public String getComplemento() {
-        return complemento;
-    }
-
-    public EnderecoDTO setComplemento(String complemento) {
-        this.complemento = complemento;
-        return this;
     }
 }
